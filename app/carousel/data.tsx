4d62063a3e6468b52bbdcdbfa64354729a814c5e
josua-stng <josuasitanggang51@gmail.com>
2023-07-20 14:29:25 +0700

    fix: product information & random recipes

--- app/carousel/data.tsx
+++ app/carousel/data.tsx
@@ -1,12 +1,10 @@
-import Image from 'next/image';
-
-import Link from 'next/link';
 import SwipperFood from './swipper';
+import RandomRecipes from '../components/random-recipes';
 
 export default async function DataSwipper() {
-  const getData = async () => {
+  const informationProducts = async () => {
     const res = await fetch(
-      `https://api.spoonacular.com/food/menuItems/search?query=burger&number=30&apiKey=${process.env.APIKEY}`
+      `${process.env.baseURL}/food/menuItems/search?query=burger&number=30&apiKey=${process.env.APIKEY}`
     );
     if (!res.ok) {
       throw new Error('Failed to fetch data');
@@ -14,51 +12,50 @@ export default async function DataSwipper() {
     return res.json();
   };
 
-  const data = await getData();
-  const slide = data.menuItems
-    ? data.menuItems.map((item: any) => item.image)
+  const randomRecipe = async () => {
+    const res = await fetch(
+      `${process.env.baseURL}/recipes/random?apiKey=${process.env.APIKEY}&number=9`
+    );
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    return res.json();
+  };
+  const randomRecipes = await randomRecipe();
+  const dataInformation = await informationProducts();
+  const slide = dataInformation.menuItems
+    ? dataInformation.menuItems.map((item: any) => item.image)
     : [];
-  const titles = data.menuItems
-    ? data.menuItems.map((item: any) => item.title)
+  const titles = dataInformation.menuItems
+    ? dataInformation.menuItems.map((item: any) => item.title)
     : [];
-  const id = data.menuItems ? data.menuItems.map((item: any) => item.id) : [];
+  const id = dataInformation.menuItems
+    ? dataInformation.menuItems.map((item: any) => item.id)
+    : [];
 
   return (
     <div>
       <SwipperFood slides={slide} title={titles} productId={id} />
-      {data.menuItems &&
-        data.menuItems.map((datas: any) => {
-          return (
-            <Link
-              href={`/product-detail/${datas.id}`}
-              key={datas.id}
-              className="flex"
-            >
-              <div>
-                <Image
-                  width={100}
-                  height={100}
-                  alt="image-data"
-                  src={datas.image}
-                  //slide={datas.id}
-                />
-              </div>
-            </Link>
-          );
-        })}
-
-      <p>Data swiper</p>
-      {/* 
-      {data.map((datas)=>{
-        return(
-           <Biasa
-        key={datas.id}
-        image={datas.image}
-        title={datas.title}      
-      /> 
-        )
-      })}
-     */}
+      <h1 className="font-bold text-2xl text-center mt-20 mb-10 font-sans">
+        Random Recipes Food
+      </h1>
+      <div className="max-w-6xl mx-auto mb-32">
+        <div className="grid grid-cols-3 justify-center gap-10">
+          {randomRecipes &&
+            randomRecipes.recipes.map((recipes: any) => {
+              return (
+                <div key={recipes.id} className="mx-auto">
+                  <RandomRecipes
+                    id={recipes.id}
+                    title={recipes.title}
+                    image={recipes.image}
+                    summary={recipes.summary}
+                  />
+                </div>
+              );
+            })}
+        </div>
+      </div>
     </div>
   );
 }
