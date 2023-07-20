8ef2ec229e440f3b7771712e74bd49390fa25d4f
josua-stng <josuasitanggang51@gmail.com>
2023-07-20 14:32:57 +0700

    create: component random recipes

---
+++ app/components/random-recipes.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function RandomRecipes({ title, image, summary, id }: any) {
+  return (
+    <Link href={`/recipe-detail/${id}`} className='hover:shadow-2x'>
+      <div className='bg-white rounded-lg shadow-xl cursor-pointer hover:shadow-2xl'>
+        <Image
+          src={image}
+          alt="random-recipe-image"
+          width={500}
+          height={250}
+          className=" rounded-md justify-center"
+          style={{ objectFit: 'cover' }}
+        />
+        <div className="p-5">
+          <p className="font-serif font-bold text-clip overflow-hidden ...">
+            {title}
+          </p>
+          {/* <p>{summary}</p> */}
+        </div>
+      </div>
+    </Link>
+  );
+}
