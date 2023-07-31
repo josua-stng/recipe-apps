0068c2c429dca8040f4859b0eeb5e4792ac67186
josua-stng <josuasitanggang51@gmail.com>
2023-07-31 10:59:50 +0700

    add: styling responsive designn

--- app/components/random-recipes.tsx
+++ app/components/random-recipes.tsx
@@ -3,21 +3,20 @@ import Link from 'next/link';
 
 export default function RandomRecipes({ title, image, summary, id }: any) {
   return (
-    <Link href={`/recipe-detail/${id}`} className='hover:shadow-2x'>
-      <div className='bg-white rounded-lg shadow-xl cursor-pointer hover:shadow-2xl'>
+    <Link href={`/recipe-detail/${id}`} className="hover:shadow-2x">
+      <div className="bg-white rounded-lg shadow-xl cursor-pointer hover:shadow-2xl max-w-xs md:max-w-sm md:mx-3">
         <Image
           src={image}
           alt="random-recipe-image"
           width={500}
           height={250}
-          className=" rounded-md justify-center"
+          className="rounded-md justify-center"
           style={{ objectFit: 'cover' }}
         />
         <div className="p-5">
           <p className="font-serif font-bold text-clip overflow-hidden ...">
             {title}
           </p>
-          {/* <p>{summary}</p> */}
         </div>
       </div>
     </Link>
