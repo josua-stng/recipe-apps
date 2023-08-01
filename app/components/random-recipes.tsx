e5bf79bdc4c3055ac50dd432db179af144b8d327
josua-stng <josuasitanggang51@gmail.com>
2023-08-01 09:50:20 +0700

    fix: random-recipes design

--- app/components/random-recipes.tsx
+++ app/components/random-recipes.tsx
@@ -4,17 +4,17 @@ import Link from 'next/link';
 export default function RandomRecipes({ title, image, summary, id }: any) {
   return (
     <Link href={`/recipe-detail/${id}`} className="hover:shadow-2x">
-      <div className="bg-white rounded-lg shadow-xl cursor-pointer hover:shadow-2xl max-w-xs md:max-w-sm md:mx-3">
+      <div className="bg-white rounded-lg shadow-xl cursor-pointer md:max-w-sm md:mx-3 group">
         <Image
           src={image}
           alt="random-recipe-image"
           width={500}
           height={250}
-          className="rounded-md justify-center"
+          className="rounded-md justify-center group-hover:scale-105"
           style={{ objectFit: 'cover' }}
         />
-        <div className="p-5">
-          <p className="font-serif font-bold text-clip overflow-hidden ...">
+        <div className="p-5 pt-7 pb-7">
+          <p className="font-serif font-bold  text-ellipsis line-clamp-1">
             {title}
           </p>
         </div>
