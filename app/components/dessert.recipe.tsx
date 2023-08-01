7aac8cfcae5a78726cdb212561909c1f597b37a6
josua-stng <josuasitanggang51@gmail.com>
2023-08-01 09:50:50 +0700

    add: dessert recipe component

---
+++ app/components/dessert.recipe.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+type Desert = {
+  id: string;
+  title: string;
+  image: string;
+};
+
+export default function Dessert({ image, title, id }: Desert) {
+  return (
+    <Link href={`/dessert-detail/${id}`}>
+      <div className="bg-white rounded-lg shadow-xl cursor-pointer md:max-w-sm md:mx-3 group">
+        <Image
+          src={image}
+          alt="random-recipe-image"
+          width={500}
+          height={250}
+          className="rounded-md justify-center group-hover:scale-105"
+          style={{ objectFit: 'cover' }}
+        />
+        <div className="p-5 pt-7 pb-7">
+          <p className="font-serif font-bold  text-ellipsis line-clamp-1">
+            {title}
+          </p>
+        </div>
+      </div>
+    </Link>
+  );
+}
