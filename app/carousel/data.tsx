3e68a364bcd8288c6e044c63e42573907697722a
josua-stng <josuasitanggang51@gmail.com>
2023-08-03 08:29:06 +0700

    feat: dessert component

--- app/carousel/data.tsx
+++ app/carousel/data.tsx
@@ -1,6 +1,7 @@
 import SwipperFood from './swipper';
 import RandomRecipes from '../components/random-recipes';
 import Dessert from '../components/dessert.recipe';
+import {CakeSlice, Cookie, Leaf } from 'lucide-react';
 
 export default async function DataSwipper() {
   const informationProducts = async () => {
@@ -33,8 +34,19 @@ export default async function DataSwipper() {
     return res.json();
   };
 
+  const vegetarianRecipe = async () => {
+    const res = await fetch(
+      `${process.env.baseURL}/recipes/complexSearch?apiKey=${process.env.APIKEY}&vegetarian=true&number=9`
+    );
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    return res.json();
+  };
+
   const randomRecipes = await randomRecipe();
   const desertRecipes = await dessertRecipe();
+  const vegetarianRecipes = await vegetarianRecipe();
   const dataInformation = await informationProducts();
   const slide = dataInformation.menuItems
     ? dataInformation.menuItems.map((item: any) => item.image)
@@ -49,30 +61,67 @@ export default async function DataSwipper() {
   return (
     <div>
       <SwipperFood slides={slide} title={titles} productId={id} />
-      <h1 className="font-bold text-2xl text-center mt-20 mb-10 font-sans">
-        Random Recipes Food
-      </h1>
-      <div className="max-w-7xl mx-auto mb-32">
+      <div className="max-w-7xl mx-auto mb-32 p-5">
+        <div className="flex items-center ml-3 mb-5">
+          <Cookie className='w-10 h-10 bg-gray-500 text-white px-2 rounded-md'/>
+          <div className='ml-3'>
+            <h1 className="font-bold text-xl text-center font-sans">
+              Random Recipe
+            </h1>
+            <p>Try random recipe</p>
+          </div>
+        </div>
         <div className="grid md:grid-cols-2  lg:grid-cols-3 justify-center gap-10">
           {randomRecipes &&
             randomRecipes.recipes.map((recipes: any) => {
               return (
                 <div key={recipes.id} className="mx-auto">
-                  <RandomRecipes
+                  <Dessert
                     id={recipes.id}
-                    title={recipes.title}
                     image={recipes.image}
-                    summary={recipes.summary}
+                    title={recipes.title}
                   />
                 </div>
               );
             })}
         </div>
       </div>
-      <h1 className="font-bold text-2xl text-center mt-20 mb-10 font-sans">
-        Dessert Land
-      </h1>
-      <div className="max-w-7xl mx-auto mb-32">
+      <div className="max-w-7xl mx-auto mb-32 p-5">
+        <div className="flex items-center ml-3 mb-5">
+          <Leaf className='w-10 h-10 bg-gray-500 text-white px-2 rounded-md'/>
+          <div className='ml-3'>
+            <h1 className="font-bold text-xl text-center font-sans">
+              The Vegetarian Food
+            </h1>
+            <p>Better for diet</p>
+          </div>
+        </div>
+        <div className="grid md:grid-cols-2  lg:grid-cols-3 justify-center gap-10">
+          {vegetarianRecipes &&
+            vegetarianRecipes.results.map((recipes: any) => {
+              return (
+                <div key={recipes.id} className="mx-auto">
+                  <Dessert
+                    id={recipes.id}
+                    image={recipes.image}
+                    title={recipes.title}
+                  />
+                </div>
+              );
+            })}
+        </div>
+      </div>
+
+      <div className="max-w-7xl mx-auto mb-32 p-5">
+      <div className="flex items-center ml-3 mb-5">
+          <CakeSlice className='w-10 h-10 bg-gray-500 text-white px-2 rounded-md'/>
+          <div className='ml-3'>
+            <h1 className="font-bold text-xl text-center font-sans">
+              The Dessert Food
+            </h1>
+            <p>Nice Dessert</p>
+          </div>
+        </div>
         <div className="grid md:grid-cols-2  lg:grid-cols-3 justify-center gap-10">
           {desertRecipes &&
             desertRecipes.results.map((recipes: any) => {
@@ -83,12 +132,6 @@ export default async function DataSwipper() {
                     image={recipes.image}
                     title={recipes.title}
                   />
-                  {/* <RandomRecipes
-                    id={recipes.id}
-                    title={recipes.title}
-                    image={recipes.image}
-                    summary={recipes.summary}
-                  /> */}
                 </div>
               );
             })}
