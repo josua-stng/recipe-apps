813204102faebbd128deba11f46d70b34ff94978
josua-stng <josuasitanggang51@gmail.com>
2023-08-04 09:34:18 +0700

    fix: swipper data

--- app/carousel/data.tsx
+++ app/carousel/data.tsx
@@ -1,19 +1,8 @@
 import SwipperFood from './swipper';
-import RandomRecipes from '../components/random-recipes';
 import Dessert from '../components/dessert.recipe';
-import {CakeSlice, Cookie, Leaf } from 'lucide-react';
+import { CakeSlice, Cookie, Leaf } from 'lucide-react';
 
 export default async function DataSwipper() {
-  const informationProducts = async () => {
-    const res = await fetch(
-      `${process.env.baseURL}/food/menuItems/search?query=burger&number=30&apiKey=${process.env.APIKEY}`
-    );
-    if (!res.ok) {
-      throw new Error('Failed to fetch data');
-    }
-    return res.json();
-  };
-
   const randomRecipe = async () => {
     const res = await fetch(
       `${process.env.baseURL}/recipes/random?apiKey=${process.env.APIKEY}&number=9`
@@ -47,24 +36,14 @@ export default async function DataSwipper() {
   const randomRecipes = await randomRecipe();
   const desertRecipes = await dessertRecipe();
   const vegetarianRecipes = await vegetarianRecipe();
-  const dataInformation = await informationProducts();
-  const slide = dataInformation.menuItems
-    ? dataInformation.menuItems.map((item: any) => item.image)
-    : [];
-  const titles = dataInformation.menuItems
-    ? dataInformation.menuItems.map((item: any) => item.title)
-    : [];
-  const id = dataInformation.menuItems
-    ? dataInformation.menuItems.map((item: any) => item.id)
-    : [];
 
   return (
     <div>
-      <SwipperFood slides={slide} title={titles} productId={id} />
+      <SwipperFood />
       <div className="max-w-7xl mx-auto mb-32 p-5">
         <div className="flex items-center ml-3 mb-5">
-          <Cookie className='w-10 h-10 bg-gray-500 text-white px-2 rounded-md'/>
-          <div className='ml-3'>
+          <Cookie className="w-10 h-10 bg-gray-500 text-white px-2 rounded-md" />
+          <div className="ml-3">
             <h1 className="font-bold text-xl text-center font-sans">
               Random Recipe
             </h1>
@@ -88,8 +67,8 @@ export default async function DataSwipper() {
       </div>
       <div className="max-w-7xl mx-auto mb-32 p-5">
         <div className="flex items-center ml-3 mb-5">
-          <Leaf className='w-10 h-10 bg-gray-500 text-white px-2 rounded-md'/>
-          <div className='ml-3'>
+          <Leaf className="w-10 h-10 bg-gray-500 text-white px-2 rounded-md" />
+          <div className="ml-3">
             <h1 className="font-bold text-xl text-center font-sans">
               The Vegetarian Food
             </h1>
@@ -113,9 +92,9 @@ export default async function DataSwipper() {
       </div>
 
       <div className="max-w-7xl mx-auto mb-32 p-5">
-      <div className="flex items-center ml-3 mb-5">
-          <CakeSlice className='w-10 h-10 bg-gray-500 text-white px-2 rounded-md'/>
-          <div className='ml-3'>
+        <div className="flex items-center ml-3 mb-5">
+          <CakeSlice className="w-10 h-10 bg-gray-500 text-white px-2 rounded-md" />
+          <div className="ml-3">
             <h1 className="font-bold text-xl text-center font-sans">
               The Dessert Food
             </h1>
