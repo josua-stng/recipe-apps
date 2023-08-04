61aaec7f282cde702aca039fbdac94a0d345fa22
josua-stng <josuasitanggang51@gmail.com>
2023-08-04 09:32:56 +0700

    add: resource image url

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:['avatars.githubusercontent.com','lh3.googleusercontent.com','spoonacular.com','images.spoonacular.com','fakestoreapi.com','pharmeasy.in']
+        domains:['avatars.githubusercontent.com','lh3.googleusercontent.com','spoonacular.com','images.spoonacular.com','fakestoreapi.com','pharmeasy.in','img.freepik.com','i.ytimg.com','graphicsfamily.com','static.vecteezy.com']
     }
 }
 
