311ad3c6b5d63a1671bc70945bcc5de4ae45ca14
josua-stng <josuasitanggang51@gmail.com>
2023-08-07 09:30:55 +0700

    fix: search recipes

--- app/header/search.tsx
+++ app/header/search.tsx
@@ -1,25 +1,14 @@
-import {
-  BuildingStorefrontIcon,
-  MagnifyingGlassIcon,
-} from '@heroicons/react/24/solid';
+import { BuildingStorefrontIcon } from '@heroicons/react/24/solid';
 import LoginPage from '../components/login-btn';
 import Link from 'next/link';
+import InputSearchProducts from './searchProduct';
 export default function Search() {
   return (
-    <div className="flex items-center border-2 border-gray-500 p-3">
-      <Link
-      href={'/'}
-      >
-      <BuildingStorefrontIcon className="w-8 h-8" />
+    <div className="flex items-center border-2 border-gray-300 p-3 ">
+      <Link href={'/'}>
+        <BuildingStorefrontIcon className="w-8 h-8" />
       </Link>
-      <div className="w-full max-w-8xl relative mr-10">
-        <input
-          type="text"
-          placeholder="search recipe"
-          className="border-2 border-gray-200 w-full max-w-8xl ml-5 h-9 px-2 rounded-sm focus:border-2 focus:border-gray-200"
-        />
-        <MagnifyingGlassIcon className="w-5 h-5 absolute top-1.5 right-1" />
-      </div>
+      <InputSearchProducts />
       <LoginPage />
     </div>
   );
